7c48510003721176ea0c37ce0fb225ee46bd851b
Owen Carruth <ocarr199@gmail.com>
2021-05-02 15:10:22 -0500

    Final Commit

--- assignment/app.js
+++ assignment/app.js
@@ -1,35 +1,37 @@
+
+// Selecting all the buttons
 let numOfBtns = document.querySelectorAll('.btn').length
 for (let i = 0; i < numOfBtns; i++) {
-
+// add event listener of click to the buttons
   document.querySelectorAll(".btn")[i].addEventListener('click', function() {
-
+//  declaring variable to decide what sound to play
     let albumClicked = this.textContent;
 
-
-      switch (albumClicked) {
-        case "Mac Miller":
-        var WhatsTheUse = new Audio('songs/WhatsTheUse.mp3');
-               WhatsTheUse.play();
-          break;
-        case "Radiohead":
-          var  WeirdFishes = new Audio(`songs/WeirdFishes.mp3`);
-          WeirdFishes.play();
-          break;
-        case "BROCKHAMPTON":
-          var FAKE = new Audio(`songs/FAKE.mp3`);
-          FAKE.play();
-          break;
-        case "Earl Sweatshirt":
-          var Riot = new Audio(`songs/Riot.mp3`);
-          Riot.play();
-          break;
-        case "Pink FLoyd":
-          var Potw = new Audio(`songs/POTW.mp3`);
-          Potw.play();
-          break;
-        default:
-    console.log(albumClicked)
-  };
+ //  switch statement for choosing sound file
+    switch (albumClicked) {
+      case "Mac Miller":
+        let WhatsTheUse = new Audio('songs/WhatsTheUse.mp3');
+        WhatsTheUse.play();
+        break;
+      case "Radiohead":
+        let WeirdFishes = new Audio(`songs/WeirdFishes.mp3`);
+        WeirdFishes.play();
+        break;
+      case "BROCKHAMPTON":
+        let FAKE = new Audio(`songs/FAKE.mp3`);
+        FAKE.play();
+        break;
+      case "Earl Sweatshirt":
+        let Riot = new Audio(`songs/Riot.mp3`);
+        Riot.play();
+        break;
+      case "Pink Floyd":
+        let Potw = new Audio(`songs/POTW.mp3`);
+        Potw.play();
+        break;
+      default:
+        console.log(albumClicked)
+    };
   });
 
-  };
+};
